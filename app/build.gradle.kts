4462d81b5e5a22d6bf4dfaf5b4a63ed382125203
razvan-eduard <razvan.sf@tuta.com>
2026-07-05 08:40:06 +0300

    fix: bump compileSdk to 37 for androidx.core:core-ktx 1.19.0 compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.voxcommander.app"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
