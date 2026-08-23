f86f3c9b0182f19c91f521fb5a9fa39bd58b9888
razvan-eduard <razvan.sn@tuta.com>
2026-08-23 17:29:06 +0300

    chore(release): expenses 0.35

--- vox-expenses/build.gradle.kts
+++ vox-expenses/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.voxapps.expenses"
         minSdk = 29
         targetSdk = 36
-        versionCode = 34
-        versionName = "0.34"
+        versionCode = 35
+        versionName = "0.35"
         ndk { abiFilters += "arm64-v8a" }
     }
 
