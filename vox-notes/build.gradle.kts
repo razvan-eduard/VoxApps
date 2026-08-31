40ce16aaced5dc44cdf04549a8bbf9cf744685ce
razvan-eduard <razvan.sn@tuta.com>
2026-08-31 13:45:48 +0300

    chore: release notes 0.29

--- vox-notes/build.gradle.kts
+++ vox-notes/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.voxapps.notes"
         minSdk = 29
         targetSdk = 36
-        versionCode = 28
-        versionName = "0.28"
+        versionCode = 29
+        versionName = "0.29"
         ndk { abiFilters += "arm64-v8a" }
     }
 
