e4eb42f0492d6cbc276fbba17f3212bb439e6583
razvan-eduard <razvan.sn@tuta.com>
2026-09-11 08:47:46 +0300

    chore: release expenses 0.50, calendar 0.32

--- vox-calendar/build.gradle.kts
+++ vox-calendar/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.voxapps.calendar"
         minSdk = 29
         targetSdk = 36
-        versionCode = 31
-        versionName = "0.31"
+        versionCode = 32
+        versionName = "0.32"
         ndk { abiFilters += "arm64-v8a" }
     }
 
